5acbbaf0ab09bcd345d3f291fae2d35e7d67a791
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-01-19 10:14:33 +0100

    Change GET to POST

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const fastify = require('fastify')({ logger: true });
 const notifyNewGrades = require('./domain/usecases/notify-new-grades');
 
-fastify.get('/', async (request, reply) => {
+fastify.post('/', async (request, reply) => {
   try {
     const result = await notifyNewGrades();
-    return reply.code(200).send(result);
+    return reply.code(200).send({ result });
   } catch (e) {
     console.error(e);
     return reply.code(500).send();
